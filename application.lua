101f15ade3520660fce8f33bbf269ae2e211430c
Karoly Molnar <karoly@lyorak.com>
2016-10-09 17:08:57 +0200

    with 1 mins delays.

--- application.lua
+++ application.lua
@@ -6,13 +6,17 @@ scl_pin = 1
 sda_pin = 2
 i2c_addr = 0xa2/2
 
+sleepminutes = 1
+sleepus = sleepminutes*60*1000000
+sleepus_min = 100 --(sleepminutes-2)*60*1000000
+
 -- initialize i2c
 i2c.setup(0, sda_pin, scl_pin, i2c.SLOW)
 
 
 -- user defined function: read from reg_addr content of dev_addr len x bytes
 function i2cReadReg(dev_addr, reg_addr,len)
-    print("dev_addr: " .. dev_addr)   
+   -- print("dev_addr: " .. dev_addr)   
     i2c.start(0)
     i2c.address(0, dev_addr, i2c.TRANSMITTER)
     i2c.write(0, reg_addr)
@@ -53,17 +57,17 @@ function readAndResetCounter()
     d01 = string.byte(counter,1)
     d23 = string.byte(counter,2)
     d45 = string.byte(counter,3)
-    print("Received count value: 0 " .. d01)
-    print("Received count value: 1 " .. d23)
-    print("Received count value: 2 " .. d45)
+ --   print("Received count value: 0 " .. d01)
+ --   print("Received count value: 1 " .. d23)
+ --   print("Received count value: 2 " .. d45)   
 
     fixed01 = tod2igits(d01)
     fixed23 = tod2igits(d23)
     fixed45 = tod2igits(d45)
     fixed = fixed01[1]+10*fixed01[2]+100*fixed23[1]+1000*fixed23[2]+10000*fixed45[1]+100000*fixed45[2]
 
-    print("fixed value: " .. fixed)
-    return fixed
+ --   print("fixed value: " .. fixed)
+    return (fixed / sleepminutes)
 end
 --mqtt_username = ""
 --mqtt_password = ""
@@ -87,8 +91,9 @@ m:on("offline", function(client) print ("offline") end)
 
 function goSleep()
     print("Now should be sleeping...")
-    gpio.write(4,gpio.HIGH)
-    --rtctime.dsleep_aligned(10*60*1000000,8*60*1000000)
+    tmr.delay(1000)
+    --gpio.write(4,gpio.HIGH)
+    rtctime.dsleep_aligned(sleepus,sleepus_min)
 end
 
 function publishToAdaIOCPM(cpm)
